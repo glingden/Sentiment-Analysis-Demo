23ca094a23ea1d07c5b76639901a5b924a4b7bc1
Ganga Lingden <lingden.ganga@gmail.com>
2019-12-20 17:10:04 +0200

    app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """
-This app predicts the given text into 3 class 1.postive, 2.neutral and , 3. Negative
+This app predicts the given text into one of 3 classes: 1.postive, 2.neutral and , 3. Negative
 Author: Ganga Lingden
 
 """
@@ -17,6 +17,7 @@ model =  pickle.load(open('Models/final_logistic', 'rb')) # predicted model
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -33,31 +34,20 @@ def predict():
 
         #check if get some value
         if text:
+            clean_text = text_cleaning(text) #clean text
+            print(clean_text)
+            matrix = tfidf_object.transform([clean_text]) #convet into matrix
+            predict_value = model.predict(matrix) # predict sentiment
+            return render_template('result.html', prediction=predict_value)
 
-            #check language
-            if len(text.split()) >3:
-                if detect(text) == 'en':
-                    clean_text = text_cleaning(text) #clean text
-                    print(clean_text)
-                    matrix = tfidf_object.transform([clean_text]) #convet into matrix
-                    predict_value = model.predict(matrix) # predict sentiment
-                    return render_template('result.html', prediction=predict_value)
-
-                else:
-                    return render_template('result.html', prediction='Only work for English Lanuage !!')
-            else:
-                clean_text = text_cleaning(text)  # clean text
-                print(clean_text)
-                matrix = tfidf_object.transform([clean_text])  # convet into matrix
-                predict_value = model.predict(matrix)  # predict sentiment
-                return render_template('result.html', prediction=predict_value)
-
+            # check language detection
+            #return render_template('result.html', prediction='Only work for English Lanuage !!')
 
         else:
-            return render_template('result.html', prediction='Please, Enter some text(reviews) !!')
+            return render_template('result.html', prediction='Please, Enter some text(reviews) !!!')
 
 
-    #if want to display result in home.html
+    #if you want to display result in home.html
     #return render_template('home.html', prediction='Predicted Sentiment: '+ clean_text)
 
 if __name__ == '__main__':
