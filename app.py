be3f846ff3e50dc8552b729d23e47d6c6f4f5ee4
Ganga Lingden <ganga.lingden@tuni.fi>
2023-03-16 23:30:02 +0200

    Updated app file

--- app.py
+++ app.py
@@ -4,51 +4,63 @@ Author: Ganga Lingden
 
 """
 
+import pickle
+import random
 
 from flask import Flask,render_template,url_for,request
 from  text_processing import  text_cleaning
-from langdetect import detect
-import pickle
-
-#load models
-tfidf_object =  pickle.load(open('Models/tfidf_vect', 'rb')) #tfidf vectorizer
-model =  pickle.load(open('Models/final_logistic', 'rb')) # predicted model
 
 
+# Load model
+tfidf_object =  pickle.load(open('Models/tfidf_vect', 'rb')) # TFIDF vectorizer
+model =  pickle.load(open('Models/final_logistic', 'rb')) # Predicted model
+
+# Randomly select between two options
+try_again_options = ["Let's try again.", "Do you want to try again?"]
+try_again = random.choice(try_again_options)
+
+
+#  Creates an instance of the Flask App
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    """Renders the home page of the web app."""
 
+    return render_template('home.html')
 
 
 @app.route('/predict',methods=['POST'])
 def predict():
-
+    """Predicts the sentiment of the given input text."""
 
     if request.method == 'POST':
-
         text = request.form['comment']
-
-        #check if get some value
+    
+        # Check if input text is provided
         if text:
-            clean_text = text_cleaning(text) #clean text
-            print(clean_text)
-            matrix = tfidf_object.transform([clean_text]) #convet into matrix
-            predict_value = model.predict(matrix) # predict sentiment
-            return render_template('result.html', prediction=predict_value)
+            # Clean text
+            clean_text = text_cleaning(text) 
 
-            # check language detection
-            #return render_template('result.html', prediction='Only work for English Lanuage !!')
+            # Convert cleaned text into a matrix using tf-idf vectorizer
+            matrix = tfidf_object.transform([clean_text])
 
+            # Predict the sentiment using trained model
+            predict_value = model.predict(matrix) 
+
+            return render_template('result.html', 
+                                   prediction=predict_value,
+                                    try_again = try_again)
+        
         else:
-            return render_template('result.html', prediction='Please, Enter some text(reviews) !!!')
+            return render_template('result.html', 
+                                    prediction='Please, Enter some text to analyse !!!',
+                                    try_again = try_again)
+        
 
 
-    #if you want to display result in home.html
-    #return render_template('home.html', prediction='Predicted Sentiment: '+ clean_text)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
